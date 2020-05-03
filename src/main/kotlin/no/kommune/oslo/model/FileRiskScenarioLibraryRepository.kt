a4df2894e1c8454f67a16698ac146981be81986f
7mann <rune.schumann@gmail.com>
2020-05-03 21:06:05 +0200

    Started working on file repository.

--- src/main/kotlin/no/kommune/oslo/model/FileRiskScenarioLibraryRepository.kt
+++ src/main/kotlin/no/kommune/oslo/model/FileRiskScenarioLibraryRepository.kt
@@ -1,11 +1,22 @@
 package no.kommune.oslo.model
 
-class FileRiskScenarioLibraryRepository : RiskScenarioLibraryRepository {
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+
+class FileRiskScenarioLibraryRepository(val fileName: String) : RiskScenarioLibraryRepository {
+    init {
+        if (fileName.isBlank()) {
+            throw IllegalArgumentException("File name must be present!")
+        }
+    }
+
     override fun readRiskScenarios(): List<RiskScenario> {
         TODO("Not yet implemented")
     }
 
     override fun writeRiskScenarios(riskScenarios: List<RiskScenario>) {
+        val gson: Gson = GsonBuilder().setPrettyPrinting().create()
+
         TODO("Not yet implemented")
     }
 }
--- src/test/kotlin/no/kommune/oslo/model/FileRiskScenarioLibraryRepositoryTest.kt
+++ src/test/kotlin/no/kommune/oslo/model/FileRiskScenarioLibraryRepositoryTest.kt
@@ -1,8 +1,22 @@
 package no.kommune.oslo.model
 
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import org.apache.logging.log4j.LogManager
 import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertFailsWith
 
 internal class FileRiskScenarioLibraryRepositoryTest {
+    private val logger = LogManager.getLogger(javaClass)
+
+    @Test
+    fun `Expect exeption thrown with illegal parameter`() {
+        assertFailsWith<IllegalArgumentException> {
+            val riskScenarioRepo = FileRiskScenarioLibraryRepository(fileName = "")
+        }
+
+    }
 
     @Test
     fun readRiskScenarios() {
@@ -10,6 +24,35 @@ internal class FileRiskScenarioLibraryRepositoryTest {
 
     @Test
     fun writeRiskScenarios() {
+        val env: MutableMap<String, String> = System.getenv()
+        logger.debug("KOZ_FILES: ${env["KOZ_FILES"]}")
+
+//        val gson = Gson()
+        val gson = GsonBuilder().setPrettyPrinting().create()
+//        val riskScenario = RiskScenario(RiskType.ASSET, "Test RiskScenario")
+        val riskScenarios = listOf(
+                RiskScenario(RiskType.ASSET, "Test RiskScenario 1 "),
+                RiskScenario(RiskType.LEGAL, "Test RiskScenario 2 "),
+                RiskScenario(RiskType.PERSONAL_DATA, "Test RiskScenario 3 "),
+                RiskScenario(RiskType.ASSET, "Test RiskScenario 4 "),
+                RiskScenario(RiskType.ASSET, "Test RiskScenario 5 ")
+        )
+
+
+        var jsonString: String = gson.toJson(riskScenarios)
+        logger.debug(jsonString)
+
+        val fileName = "${env["KOZ_FILES"]}/test.json"
+        val file = File(fileName)
+        file.writeText(jsonString)
+
+
+        val arrayRiskScenarioType = object : TypeToken<List<RiskScenario>>() {}.type
+        var riskScenarioList: List<RiskScenario> = gson.fromJson(jsonString, arrayRiskScenarioType)
+        logger.debug(riskScenarioList)
+
+
+//        gson.toJson(riskScenario, File("${env["KOZ_FILES"]}/test.json").writer())
 
     }
 }
